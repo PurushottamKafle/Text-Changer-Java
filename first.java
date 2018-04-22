a023b0dc0320ccd0a17139626a4e2ffe0ab786e7
Purushottam Kafle <32263675+PurushottamKafle@users.noreply.github.com>
2018-04-22 20:36:58 +0545

    Create first.java

---
+++ first.java
@@ -0,0 +1,14 @@
+package pksoft;
+
+import javax.swing.JFrame;
+
+public class first  { 
+
+	public static void main(String args[]) {
+second s = new second();
+s.setSize(300,200);
+s.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+s.setVisible(true);
+	}
+	
+}
